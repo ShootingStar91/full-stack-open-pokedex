52af32fbb69910afb24e46211aadab47dd750d10
arttu <ttu91@hotmail.com>
2022-02-13 17:03:30 +0200

    e2e should work...

--- cypress/integration/poke_app.spec.js
+++ cypress/integration/poke_app.spec.js
@@ -6,4 +6,12 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('single pokemon page works', function() {
+    cy.visit('http://localhost:5000')
+    cy.get('a[href*="ivysaur"]').click()
+    cy.get('*[class^="pokemon-abilities"]')
+    .get('*[class^="pokemon-ability"]')
+    .get('*[class^="pokemon-ability-name"]').contains('chlorophyll')
+  })
 })
